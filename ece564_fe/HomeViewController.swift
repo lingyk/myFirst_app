b197a04775232684b6a2bd49b013a831ce05ccbe
Leii <596878919@qq.com>
2022-04-13 00:20:47 +0800

    creat viewcontrollers

---
+++ ece564_fe/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  ece564_fe
+//
+//  Created by Lu Yu on 2022/4/13.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var resultLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if  segue.identifier == "toCalculatorPage",
+            let vc = segue.destination as? CalculatorViewController{
+            vc.resultHandler = { text in
+                self.resultLabel.text = text
+            }
+        }
+    }
+
+}
