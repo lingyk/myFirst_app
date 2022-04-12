450cb6726f2714bb9abe50bf39776b8be70cfa20
Leii <596878919@qq.com>
2022-04-13 00:58:12 +0800

    calculator number

--- ece564_fe/CalculatorViewController.swift
+++ ece564_fe/CalculatorViewController.swift
@@ -33,21 +33,80 @@ class CalculatorViewController: UIViewController {
     
     @IBOutlet weak var clearOperator: UIButton!
     @IBOutlet weak var equalOperator: UIButton!
+    
+    var numBtnList = [UIButton]()
+    var operatorBtnList = [UIButton]()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        showLabel.text = ""
+        numBtnList = [zeroBtn,oneBtn,twoBtn,threeBtn,fourBtn,fiveBtn,sixBtn,sevenBtn,eightBtn,nineBtn]
+        for (index,item) in numBtnList.enumerated() {
+            item.tag = index
+            item.addTarget(self, action: #selector(clickNum(btn:)), for: .touchUpInside)
+        }
+        operatorBtnList = [addOperator,subtractionOperator,multiplicationOperator,divisionOperator]
+        
+        for (index,item) in operatorBtnList.enumerated() {
+            item.tag = index
+            item.addTarget(self, action: #selector(clickOperator(btn:)), for: .touchUpInside)
+        }
+        clearOperator.addTarget(self, action: #selector(clickClearOperator(btn:)), for: .touchUpInside)
+        equalOperator.addTarget(self, action: #selector(clickEqualOperator(btn:)), for: .touchUpInside)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func clickNum(btn:UIButton)  {
+        let text = btn.titleLabel!.text!
+        showLabel.text = showLabel.text! + text
     }
-    */
-
+    
+    @objc func clickOperator(btn:UIButton)  {
+        let text = btn.titleLabel!.text!
+        showLabel.text = showLabel.text! + text
+    }
+    @objc func clickClearOperator(btn:UIButton)  {
+       
+        showLabel.text = ""
+    }
+    @objc func clickEqualOperator(btn:UIButton)  {
+        let text = showLabel.text!
+        if text.contains("*") {
+            let list = text.components(separatedBy: "*")
+            if list.count == 2,
+               let first = Int(list.first!),
+               let last = Int(list.last!){
+                showLabel.text = text + "=" + String(first * last)
+            }
+        }else if text.contains("/") {
+            let list = text.components(separatedBy: "/")
+            if list.count == 2,
+               let first = Int(list.first!),
+               let last = Int(list.last!),
+                last != 0{
+                showLabel.text = text + "=" + String(first / last)
+            }
+        }else if text.contains("+") {
+            let list = text.components(separatedBy: "+")
+            if list.count == 2,
+               let first = Int(list.first!),
+               let last = Int(list.last!){
+                showLabel.text = text + "=" + String(first + last)
+            }
+        } else if text.contains("-") {
+            let list = text.components(separatedBy: "-")
+            if list.count == 2,
+               let first = Int(list.first!),
+               let last = Int(list.last!){
+                showLabel.text = text + "=" + String(first - last)
+            }
+        }
+        if showLabel.text!.contains("="),
+           let handler = self.resultHandler {
+            handler(showLabel.text!)
+            
+        }else{
+            print("operator error")
+        }
+        
+    }
 }
