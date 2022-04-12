00818cfc6f9a61f13ce97efc5c9fd644bfd7e06c
Leii <596878919@qq.com>
2022-04-13 00:39:50 +0800

    CalculatorPage  ui create

--- ece564_fe/CalculatorViewController.swift
+++ ece564_fe/CalculatorViewController.swift
@@ -9,6 +9,30 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     var resultHandler : ((_ text:String)->())?
+    
+    @IBOutlet weak var showLabel: UILabel!
+    @IBOutlet weak var zeroBtn: UIButton!
+    @IBOutlet weak var oneBtn: UIButton!
+    @IBOutlet weak var twoBtn: UIButton!
+    
+    @IBOutlet weak var threeBtn: UIButton!
+    @IBOutlet weak var fourBtn: UIButton!
+    @IBOutlet weak var fiveBtn: UIButton!
+    
+    @IBOutlet weak var sixBtn: UIButton!
+    @IBOutlet weak var sevenBtn: UIButton!
+    @IBOutlet weak var eightBtn: UIButton!
+    @IBOutlet weak var nineBtn: UIButton!
+    
+    
+    @IBOutlet weak var addOperator: UIButton!
+    
+    @IBOutlet weak var subtractionOperator: UIButton!
+    @IBOutlet weak var multiplicationOperator: UIButton!
+    @IBOutlet weak var divisionOperator: UIButton!
+    
+    @IBOutlet weak var clearOperator: UIButton!
+    @IBOutlet weak var equalOperator: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
